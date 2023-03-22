91426bddd0883915c7c866ff046a6d854254cc4a
gpbaculio <gpbaculio@gmail.com>
2023-03-23 00:06:02 +0800

    initialize sqlite

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,45 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Alert} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {createTable, getMenuItems} from './database';
+import {SectionDataType} from './utils';
+
+const API_URL =
+  'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json';
+
+const fetchData = async () => {
+  // 1. Implement this function
+  const data = await fetch(API_URL).then(response => response.json());
+
+  // Fetch the menu from the API_URL endpoint. You can visit the API_URL in your browser to inspect the data returned
+  // The category field comes as an object with a property called "title". You just need to get the title value and set it under the key "category".
+  // So the server response should be slighly transformed in this function (hint: map function) to flatten out each menu item in the array,
+  return [...data.menu];
+};
 
 const Home = () => {
+  const [data, setData] = useState<SectionDataType[]>([]);
+
+  useEffect(() => {
+    const didMount = async () => {
+      try {
+        await createTable();
+        let menuItems = await getMenuItems();
+        // useDevLogData(menuItems, "getMenuItems()");
+        // The application only fetches the menu data once from a remote URL
+        // and then stores it into a SQLite database.
+        // After that, every application restart loads the menu from the database
+        if (!menuItems.length) {
+          menuItems = await fetchData();
+        }
+      } catch (e) {
+        // Handle error
+        Alert.alert((e as Error).message);
+      }
+    };
+
+    didMount();
+  }, []);
+
   return (
     <View>
       <Text>Home</Text>
