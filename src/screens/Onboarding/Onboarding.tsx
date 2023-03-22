be6e8be18f5d1697ca8229b79d749193c18c2291
gpbaculio <gpbaculio@gmail.com>
2023-03-22 18:07:22 +0800

    finish profile ui

--- src/screens/Onboarding/Onboarding.tsx
+++ src/screens/Onboarding/Onboarding.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { KeyboardAvoidingView, ScrollView, StyleSheet } from "react-native";
+import React from 'react';
 
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Controller, useForm } from "react-hook-form";
-import * as Yup from "yup";
+import {yupResolver} from '@hookform/resolvers/yup';
+import {Controller, useForm} from 'react-hook-form';
+import * as Yup from 'yup';
 
 import {
   DynamicView,
   DynamicText,
   DynamicTextInput,
   DynamicPressable,
-} from "src/components";
+  KeyboardScroll,
+} from 'src/components';
 
-import { useRootNavigation } from "src/navigation/hooks";
+import {useRootNavigation} from 'src/navigation/hooks';
 
-type OnboardingFormData = { firstName: string; email: string };
+type OnboardingFormData = {firstName: string; email: string};
 
 export const handleFormColor = (isError: boolean) =>
-  isError ? "red" : "#495E57";
+  isError ? 'red' : '#495E57';
 
 const Onboarding = () => {
   const navigation = useRootNavigation();
@@ -25,134 +25,121 @@ const Onboarding = () => {
   const {
     control,
     handleSubmit,
-    formState: { errors },
+    formState: {errors},
   } = useForm<OnboardingFormData>({
     resolver,
-    mode: "onChange",
+    mode: 'onChange',
     defaultValues: {
-      firstName: "sss",
-      email: "as@gmail.com",
+      firstName: 'sss',
+      email: 'as@gmail.com',
     },
   });
 
   const hasErrors = !!Object.keys(errors).length;
 
-  const onSubmit = ({ firstName, email }: OnboardingFormData) => {
-    navigation.navigate("Profile");
+  const onSubmit = ({firstName, email}: OnboardingFormData) => {
+    navigation.navigate('Profile');
   };
 
   return (
-    <KeyboardAvoidingView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.content}>
-        <DynamicView
-          flex={1}
-          alignItems='center'
-          backgroundColor='#AFAFAF'
-          justifyContent='space-between'
-          paddingVertical='xL'>
-          <DynamicText
-            mt='xxL'
-            mb='l'
-            color='#495E57'
-            fontWeight='600'
-            fontSize={21}>
-            Let us get to know you
-          </DynamicText>
-          <DynamicView width='100%'>
-            <DynamicView
-              variant='center'
-              alignItems='center'
-              paddingHorizontal='xL'>
-              <DynamicText variant='formLabel'>First Name</DynamicText>
-              <Controller
-                control={control}
-                rules={{ required: true }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                  <DynamicTextInput
-                    color={handleFormColor(!!errors.firstName)}
-                    borderColor={handleFormColor(!!errors.firstName)}
-                    variant='input'
-                    placeholder='First Name'
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                  />
-                )}
-                name='firstName'
-              />
-              {!!errors.firstName ? (
-                <DynamicText variant='error'>
-                  {errors.firstName.message}
-                </DynamicText>
-              ) : null}
-            </DynamicView>
-            <DynamicView
-              mt='l'
-              variant='center'
-              alignItems='center'
-              width='100%'
-              paddingHorizontal='xL'>
-              <DynamicText variant='formLabel'>Email</DynamicText>
-              <Controller
-                control={control}
-                rules={{ required: true }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                  <DynamicTextInput
-                    color={handleFormColor(!!errors.email)}
-                    borderColor={handleFormColor(!!errors.email)}
-                    variant='input'
-                    placeholder='Email'
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                  />
-                )}
-                name='email'
-              />
-              {!!errors.email ? (
-                <DynamicText variant='error'>
-                  {errors.email.message}
-                </DynamicText>
-              ) : null}
-            </DynamicView>
+    <KeyboardScroll>
+      <DynamicView
+        flex={1}
+        alignItems="center"
+        backgroundColor="#AFAFAF"
+        justifyContent="space-between"
+        paddingVertical="xL">
+        <DynamicText
+          mt="xxL"
+          mb="l"
+          color="#495E57"
+          fontWeight="600"
+          fontSize={21}>
+          Let us get to know you
+        </DynamicText>
+        <DynamicView width="100%">
+          <DynamicView
+            variant="center"
+            alignItems="center"
+            paddingHorizontal="xL">
+            <DynamicText variant="formLabel">First Name</DynamicText>
+            <Controller
+              control={control}
+              rules={{required: true}}
+              render={({field: {onChange, onBlur, value}}) => (
+                <DynamicTextInput
+                  color={handleFormColor(!!errors.firstName)}
+                  borderColor={handleFormColor(!!errors.firstName)}
+                  variant="input"
+                  placeholder="First Name"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="firstName"
+            />
+            {!!errors.firstName ? (
+              <DynamicText variant="error">
+                {errors.firstName.message}
+              </DynamicText>
+            ) : null}
+          </DynamicView>
+          <DynamicView
+            mt="l"
+            variant="center"
+            alignItems="center"
+            width="100%"
+            paddingHorizontal="xL">
+            <DynamicText variant="formLabel">Email</DynamicText>
+            <Controller
+              control={control}
+              rules={{required: true}}
+              render={({field: {onChange, onBlur, value}}) => (
+                <DynamicTextInput
+                  color={handleFormColor(!!errors.email)}
+                  borderColor={handleFormColor(!!errors.email)}
+                  variant="input"
+                  placeholder="Email"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="email"
+            />
+            {!!errors.email ? (
+              <DynamicText variant="error">{errors.email.message}</DynamicText>
+            ) : null}
           </DynamicView>
         </DynamicView>
-        <DynamicView paddingVertical='xxL'>
-          <DynamicPressable
-            disabled={hasErrors}
-            opacity={hasErrors ? 0.5 : 1}
-            onPress={handleSubmit(onSubmit)}
-            ml='auto'
-            width={150}
-            variant='button'
-            backgroundColor='#AFAFAF'
-            mr='xL'>
-            <DynamicText color='#495E57' fontWeight='500'>
-              Next
-            </DynamicText>
-          </DynamicPressable>
-        </DynamicView>
-      </ScrollView>
-    </KeyboardAvoidingView>
+      </DynamicView>
+      <DynamicView paddingVertical="xxL">
+        <DynamicPressable
+          disabled={hasErrors}
+          opacity={hasErrors ? 0.5 : 1}
+          onPress={handleSubmit(onSubmit)}
+          ml="auto"
+          width={150}
+          variant="button"
+          backgroundColor="#AFAFAF"
+          mr="xL">
+          <DynamicText color="#495E57" fontWeight="500">
+            Next
+          </DynamicText>
+        </DynamicPressable>
+      </DynamicView>
+    </KeyboardScroll>
   );
 };
 
 const resolver = yupResolver(
   Yup.object({
-    firstName: Yup.string().required("First Name is required"),
+    firstName: Yup.string().required('First Name is required'),
     email: Yup.string()
-      .email("Invalid Email Address")
-      .required("Email is required"),
-  })
+      .email('Invalid Email Address')
+      .required('Email is required'),
+  }),
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  content: {
-    flexGrow: 1,
-  },
-});
 
 export default Onboarding;
--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,43 +1,265 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {View, Text} from 'react-native';
+import React, {useState} from 'react';
 
-import { Controller, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as Yup from "yup";
-import "yup-phone";
-import { DynamicPressable, DynamicText, DynamicView } from "src/components";
+import {Controller, useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import 'yup-phone';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {
+  DynamicMaskedTextInput,
+  DynamicPressable,
+  DynamicText,
+  DynamicTextInput,
+  DynamicView,
+  KeyboardScroll,
+} from 'src/components';
+import {handleFormColor} from '../Onboarding/Onboarding';
+import {MaskedTextInput} from 'react-native-mask-text';
+
+type ProfileFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+};
 
 const Profile = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<ProfileFormData>({
+    resolver,
+    mode: 'onChange',
+    defaultValues: {
+      firstName: 'Phil Dominic',
+      lastName: 'Baculio',
+      email: 'gpbaculio@gmail.com',
+      phoneNumber: '09177335242',
+    },
+  });
+  const [toggleCheck, setToggleCheck] = useState(false);
   return (
-    <DynamicView flex={1} padding='m'>
-      <DynamicText fontSize={18} fontWeight='bold'>
-        Personal Information
-      </DynamicText>
-      <DynamicView mt='l' flexDirection='row' alignItems='center'>
-        <DynamicView mr='l'>
-          <DynamicText variant='profileLabel' color='#AFAFAF'>
-            Avatar
-          </DynamicText>
-          <DynamicView
-            mt='xxs'
-            width={60}
-            height={60}
-            borderRadius={60}
-            bg='#57B87D'
-          />
+    <KeyboardScroll>
+      <DynamicView flex={1} padding="m">
+        <DynamicText fontSize={18} fontWeight="bold">
+          Personal Information
+        </DynamicText>
+        <DynamicView mt="xxs" flexDirection="row" alignItems="center">
+          <DynamicView mr="l">
+            <DynamicText variant="profileLabel" color="#AFAFAF">
+              Avatar
+            </DynamicText>
+            <DynamicView
+              mt="xxs"
+              width={60}
+              height={60}
+              borderRadius={60}
+              bg="#57B87D"
+            />
+          </DynamicView>
+          <DynamicPressable
+            mt="xL"
+            padding="xxs"
+            borderRadius={8}
+            mr="l"
+            bg="#495E57">
+            <DynamicText color="#FFFFFF">Change</DynamicText>
+          </DynamicPressable>
+          <DynamicPressable
+            mt="xL"
+            padding="xxs"
+            borderRadius={8}
+            borderColor="#495E57"
+            borderWidth={1}>
+            <DynamicText color="#495E57">Remove</DynamicText>
+          </DynamicPressable>
         </DynamicView>
-        <DynamicPressable padding='xxs' borderRadius={8} mr='l' bg='#495E57'>
-          <DynamicText color='#FFFFFF'>Change</DynamicText>
-        </DynamicPressable>
+        <DynamicView>
+          <DynamicView variant="center">
+            <DynamicText variant="profileInputLabel">First Name</DynamicText>
+            <Controller
+              control={control}
+              rules={{required: true}}
+              render={({field: {onChange, onBlur, value}}) => (
+                <DynamicTextInput
+                  color={handleFormColor(!!errors.firstName)}
+                  borderColor={handleFormColor(!!errors.firstName)}
+                  variant="profileInput"
+                  placeholder="First Name"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="firstName"
+            />
+            {!!errors.firstName ? (
+              <DynamicText variant="error">
+                {errors.firstName.message}
+              </DynamicText>
+            ) : null}
+          </DynamicView>
+          <DynamicView variant="center">
+            <DynamicText variant="profileInputLabel">Last Name</DynamicText>
+            <Controller
+              control={control}
+              rules={{required: true}}
+              render={({field: {onChange, onBlur, value}}) => (
+                <DynamicTextInput
+                  color={handleFormColor(!!errors.lastName)}
+                  borderColor={handleFormColor(!!errors.lastName)}
+                  variant="profileInput"
+                  placeholder="Last Name"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="lastName"
+            />
+            {!!errors.lastName ? (
+              <DynamicText variant="error">
+                {errors.lastName.message}
+              </DynamicText>
+            ) : null}
+          </DynamicView>
+          <DynamicView variant="center">
+            <DynamicText variant="profileInputLabel">Email</DynamicText>
+            <Controller
+              control={control}
+              rules={{required: true}}
+              render={({field: {onChange, onBlur, value}}) => (
+                <DynamicTextInput
+                  color={handleFormColor(!!errors.email)}
+                  borderColor={handleFormColor(!!errors.email)}
+                  variant="profileInput"
+                  placeholder="Email"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="email"
+            />
+            {!!errors.email ? (
+              <DynamicText variant="error">{errors.email.message}</DynamicText>
+            ) : null}
+          </DynamicView>
+          <DynamicView variant="center">
+            <DynamicText variant="profileInputLabel">Phone number</DynamicText>
+            <Controller
+              control={control}
+              rules={{required: true}}
+              render={({field: {onChange, onBlur, value}}) => (
+                <MaskedTextInput
+                  mask="(9999) 999-9999"
+                  style={{
+                    borderWidth: 1,
+                    paddingVertical: 4,
+                    width: '100%',
+                    paddingHorizontal: 8,
+                    borderRadius: 8,
+                    color: handleFormColor(!!errors.phoneNumber),
+                    borderColor: handleFormColor(!!errors.phoneNumber),
+                  }}
+                  placeholder="Phone number"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="phoneNumber"
+            />
+            {!!errors.phoneNumber ? (
+              <DynamicText variant="error">
+                {errors.phoneNumber.message}
+              </DynamicText>
+            ) : null}
+          </DynamicView>
+        </DynamicView>
+        <DynamicText mt="m" fontSize={18} fontWeight="bold">
+          Email Notifications
+        </DynamicText>
+        <DynamicView>
+          <DynamicPressable
+            marginVertical="xxxs"
+            flexDirection="row"
+            alignItems="center"
+            onPress={() => setToggleCheck(v => !v)}>
+            <Ionicons
+              name={toggleCheck ? 'ios-checkbox' : 'square-outline'}
+              size={23}
+              color="#495E57"
+            />
+            <DynamicText ml="xs">Order statuses</DynamicText>
+          </DynamicPressable>
+          <DynamicPressable
+            marginVertical="xxxs"
+            flexDirection="row"
+            alignItems="center"
+            onPress={() => setToggleCheck(v => !v)}>
+            <Ionicons
+              name={toggleCheck ? 'ios-checkbox' : 'square-outline'}
+              size={23}
+              color="#495E57"
+            />
+            <DynamicText ml="xs">Order statuses</DynamicText>
+          </DynamicPressable>
+          <DynamicPressable
+            marginVertical="xxxs"
+            flexDirection="row"
+            alignItems="center"
+            onPress={() => setToggleCheck(v => !v)}>
+            <Ionicons
+              name={toggleCheck ? 'ios-checkbox' : 'square-outline'}
+              size={23}
+              color="#495E57"
+            />
+            <DynamicText ml="xs">Order statuses</DynamicText>
+          </DynamicPressable>
+          <DynamicPressable
+            marginVertical="xxxs"
+            flexDirection="row"
+            alignItems="center"
+            onPress={() => setToggleCheck(v => !v)}>
+            <Ionicons
+              name={toggleCheck ? 'ios-checkbox' : 'square-outline'}
+              size={23}
+              color="#495E57"
+            />
+            <DynamicText ml="xs">Order statuses</DynamicText>
+          </DynamicPressable>
+        </DynamicView>
         <DynamicPressable
-          padding='xxs'
-          borderRadius={8}
-          borderColor='#495E57'
-          borderWidth={1}>
-          <DynamicText color='#495E57'>Remove</DynamicText>
+          marginVertical="s"
+          backgroundColor="#F4CE14"
+          variant="button">
+          <DynamicText fontWeight="bold">Log out</DynamicText>
         </DynamicPressable>
       </DynamicView>
-    </DynamicView>
+      <DynamicView
+        mb="xxL"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-around">
+        <DynamicPressable
+          variant="button"
+          borderWidth={1}
+          borderColor="#495E57">
+          <DynamicText fontWeight="bold" color="#AFAFAF">
+            Log out
+          </DynamicText>
+        </DynamicPressable>
+        <DynamicPressable variant="button" backgroundColor="#495E57">
+          <DynamicText fontWeight="bold" color="#FFFFFF">
+            Log out
+          </DynamicText>
+        </DynamicPressable>
+      </DynamicView>
+    </KeyboardScroll>
   );
 };
 
@@ -46,16 +268,16 @@ export default Profile;
 const resolver = yupResolver(
   Yup.object({
     firstName: Yup.string()
-      .min(2, "Please enter a valid First Name")
-      .required("First Name is required"),
+      .min(2, 'Please enter a valid First Name')
+      .required('First Name is required'),
     lastName: Yup.string()
-      .min(2, "Please enter a valid Last Name")
-      .required("Last Name is required"),
+      .min(2, 'Please enter a valid Last Name')
+      .required('Last Name is required'),
     email: Yup.string()
-      .email("Invalid Email Address")
-      .required("Email is required"),
+      .email('Invalid Email Address')
+      .required('Email is required'),
     phoneNumber: Yup.string()
-      .phone("US", true, "Please enter a Valid Phone number")
-      .required("Please enter a Valid Phone number"),
-  })
+      .phone('PH', true, 'Please enter a Valid Phone number')
+      .required('Please enter a Valid Phone number'),
+  }),
 );
