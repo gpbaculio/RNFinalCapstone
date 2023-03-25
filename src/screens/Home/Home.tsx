8cd3075bca7441756ca0379f007da5eca6e706a8
gpbaculio <gpbaculio@gmail.com>
2023-03-25 09:22:12 +0800

    fix ui

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -63,98 +63,102 @@ const Home = () => {
   const pills = ['Lunch', 'Mains', 'Desserts', 'A La Carte', 'Specials'];
 
   return (
-    <DynamicView flex={1}>
-      <DynamicView p="l" backgroundColor="#495E57">
-        <DynamicText fontSize={36} color="#F4CE14" fontWeight="500">
-          Little Lemon
-        </DynamicText>
-        <DynamicView mt="xxs" flexDirection="row">
-          <DynamicView pt="xL" flex={1}>
-            <DynamicView position="absolute">
-              <DynamicText fontSize={24} color="#FFFFFF" fontWeight="500">
-                Chicago
-              </DynamicText>
-            </DynamicView>
-            <DynamicText mt="m" fontSize={16} color="#FFFFFF" fontWeight="500">
-              We are a family owned Mediterranean restaurant, focused on
-              traditional recipes served with a modern twist.
+    <FlatList
+      nestedScrollEnabled
+      data={menuData}
+      ListHeaderComponent={() => (
+        <DynamicView>
+          <DynamicView p="l" backgroundColor="#495E57">
+            <DynamicText fontSize={36} color="#F4CE14" fontWeight="500">
+              Little Lemon
             </DynamicText>
-          </DynamicView>
-          <DynamicImage ml="m" source={homeImg} width={140} height={150} />
-        </DynamicView>
-        <DynamicView
-          mt="l"
-          backgroundColor="#D9D9D9"
-          padding="xxs"
-          width={36}
-          height={36}
-          borderRadius={36}
-          variant="center"
-          mr="auto">
-          <FontAwesome name="search" size={18} color="#333333" />
-        </DynamicView>
-      </DynamicView>
-      <DynamicView pt="l">
-        <DynamicText pl="l" mb="s" fontWeight="700" fontSize={18}>
-          ORDER FOR DELIVERY
-        </DynamicText>
-        <FlatList
-          showsHorizontalScrollIndicator={false}
-          horizontal
-          contentContainerStyle={{paddingLeft: 16}}
-          data={pills}
-          ItemSeparatorComponent={() => <DynamicView width={10} />}
-          renderItem={({item}) => (
-            <DynamicView variant="orderPill">
-              <DynamicText variant="orderPill">{item}</DynamicText>
-            </DynamicView>
-          )}
-        />
-        <DynamicView paddingHorizontal="l" mt="l">
-          <DynamicView height={1} backgroundColor="#D9D9D9" />
-        </DynamicView>
-      </DynamicView>
-      <FlatList
-        data={menuData}
-        contentContainerStyle={{padding: 16}}
-        ItemSeparatorComponent={() => (
-          <DynamicView
-            height={1}
-            backgroundColor="#D9D9D9"
-            marginVertical="xs"
-          />
-        )}
-        renderItem={({item}) => (
-          <DynamicView>
-            <DynamicText
-              fontSize={18}
-              mb="xxs"
-              color="#000000"
-              fontWeight="700">
-              {item.name}
-            </DynamicText>
-            <DynamicView flexDirection="row">
-              <DynamicView flex={1}>
-                <DynamicText fontWeight="400" fontSize={16} color="#495E57">
-                  {item.description}
-                </DynamicText>
-                <DynamicText fontWeight="500" fontSize={18} color="#495E57">
-                  ${item.price}
+            <DynamicView mt="xxs" flexDirection="row">
+              <DynamicView pt="xL" flex={1}>
+                <DynamicView position="absolute">
+                  <DynamicText fontSize={24} color="#FFFFFF" fontWeight="500">
+                    Chicago
+                  </DynamicText>
+                </DynamicView>
+                <DynamicText
+                  mt="m"
+                  fontSize={16}
+                  color="#FFFFFF"
+                  fontWeight="500">
+                  We are a family owned Mediterranean restaurant, focused on
+                  traditional recipes served with a modern twist.
                 </DynamicText>
               </DynamicView>
-              <DynamicImage
-                source={{
-                  uri: `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${item.image}?raw=true`,
-                }}
-                ml="s"
-                width={83}
-                height={83}
-              />
+              <DynamicImage ml="m" source={homeImg} width={140} height={150} />
+            </DynamicView>
+            <DynamicView
+              mt="l"
+              backgroundColor="#D9D9D9"
+              padding="xxs"
+              width={36}
+              height={36}
+              borderRadius={36}
+              variant="center"
+              mr="auto">
+              <FontAwesome name="search" size={18} color="#333333" />
             </DynamicView>
           </DynamicView>
-        )}
-      />
-    </DynamicView>
+          <DynamicView pt="l">
+            <DynamicText pl="l" mb="s" fontWeight="700" fontSize={18}>
+              ORDER FOR DELIVERY
+            </DynamicText>
+            <FlatList
+              nestedScrollEnabled
+              showsHorizontalScrollIndicator={false}
+              horizontal
+              contentContainerStyle={{paddingLeft: 16}}
+              data={pills}
+              ItemSeparatorComponent={() => <DynamicView width={10} />}
+              renderItem={({item}) => (
+                <DynamicView variant="orderPill">
+                  <DynamicText variant="orderPill">{item}</DynamicText>
+                </DynamicView>
+              )}
+            />
+            <DynamicView paddingHorizontal="l" mt="l">
+              <DynamicView height={1} backgroundColor="#D9D9D9" />
+            </DynamicView>
+          </DynamicView>
+        </DynamicView>
+      )}
+      ItemSeparatorComponent={() => (
+        <DynamicView
+          height={1}
+          backgroundColor="#D9D9D9"
+          marginVertical="xs"
+          marginHorizontal="l"
+        />
+      )}
+      renderItem={({item}) => (
+        <DynamicView p="l">
+          <DynamicText fontSize={18} mb="xxs" color="#000000" fontWeight="700">
+            {item.name}
+          </DynamicText>
+          <DynamicView flexDirection="row">
+            <DynamicView flex={1}>
+              <DynamicText fontWeight="400" fontSize={16} color="#495E57">
+                {item.description}
+              </DynamicText>
+              <DynamicText fontWeight="500" fontSize={18} color="#495E57">
+                ${item.price}
+              </DynamicText>
+            </DynamicView>
+            <DynamicImage
+              source={{
+                uri: `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${item.image}?raw=true`,
+              }}
+              ml="s"
+              width={83}
+              height={83}
+            />
+          </DynamicView>
+        </DynamicView>
+      )}
+    />
   );
 };
 
